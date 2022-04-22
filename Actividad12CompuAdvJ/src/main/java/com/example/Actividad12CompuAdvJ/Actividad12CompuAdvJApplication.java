866a7a93b618dc194bb64dd6b6aabafd9fbc33af
DrGrodus <ebermd00@gmail.com>
2022-04-22 04:34:18 -0500

    se agrego el proyecto y se creo la clase Persona

---
+++ Actividad12CompuAdvJ/src/main/java/com/example/Actividad12CompuAdvJ/Actividad12CompuAdvJApplication.java
@@ -0,0 +1,13 @@
+package com.example.Actividad12CompuAdvJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad12CompuAdvJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad12CompuAdvJApplication.class, args);
+	}
+
+}
